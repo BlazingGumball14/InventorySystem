1f171e1e347c86610b787d5e0a5d3780b01b8f2c
DaRealDaryl <92732761+DaRealDaryl@users.noreply.github.com>
2021-10-27 02:08:57 +0800

    Functional Main Function (miniproj)
    I still have not done Add function. I think there is no need for us to separate functions by file. Just put them all in one program (in this case, I propose the miniproj file)

---
+++ miniproj.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void load_menu(void);
+void addItem(void);
+//void rest(void);
+ 
+int main(int argc, char** argv)
+{
+    load_menu();
+    return 0;
+}
+ 
+void load_menu(void)
+{
+    char choice;
+ 	int inv;
+ 	
+    do
+    {
+    	system("cls"); //this is for clearing cmd screen
+    	inv = 0;
+        printf("MAIN MENU\n\nAdd Inventort Item\n");
+        printf("[A] Sum\n");
+        printf("[B] Rest\n");
+        printf("[X] Exit\n");
+        printf("\nPlease input choice: ");
+        scanf("%c", &choice);
+ 
+        switch(choice)
+        {
+            case 'A': case 'a': addItem();
+                break;
+            /* Edit your function
+			case 'B': case 'b': //rest(); //that rest function is from the original code, which I used as test function 2
+                break;
+            case 'C': case 'c': 
+            	break;
+            case 'D': case 'd': 
+            	break;
+            case 'E': case 'e': 
+            	break;
+            */
+            case 'X': case 'x': printf("Quitting program!\n");
+            	choice != 'X'; //part of the failsafe while at the end
+                exit(0); //program should exit here
+                break;
+            default: printf("Invalid choice!\n\n");
+            	inv = 1;
+                break;
+        }
+ 
+ 		//reproducing what Klars did to resolve same bug concept
+ 		if(inv == 1 )
+ 		{
+ 			printf("Press Enter to continue\n");
+ 			scanf("%c", &choice);
+ 			
+ 		}
+ 			
+    } while (choice != 'X'); //the program should close on exit(0) on the exit case 'X/x'. this here is a failsafe
+ 
+}
+ 
+void addItem(void)
+{
+    int num1, num2;
+    int ch;
+     
+    printf("Enter number 1: ");
+    scanf("%d",&num1);
+    printf("Enter number 2: ");
+    scanf("%d",&num2);
+ 
+    printf("\nThe sum of the numbers was: %d",num1+num2);
+ 
+    /* Flushes input buffer from the newline from scanf() */
+    while ( (ch = getchar()) != '\n' && ch != EOF) ;
+ 
+    printf("\n\nPress ENTER to continue.\n");
+    while ( (ch = getchar()) != '\n' && ch != EOF)
+        ;
+ 
+    return;
+}       
+ 
+ /* this is from the original code I copied
+ 	feel free to enable it to experiment a bit if you want
+ 
+void rest(void)
+{
+    int ch;
+    printf("Sleepy sleepy... zZZzZzZz\n");
+    printf("You now feel awake again!\n");
+ 
+    // Flushes input buffer 
+    while ((ch = getchar()) != '\n' && ch != EOF) ;
+ 
+    printf("\n\nPress ENTER to continue.\n");
+    while ((ch = getchar()) != '\n' && ch != EOF)
+        ;
+ 
+    return;
+}
+*/
