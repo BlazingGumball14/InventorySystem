a0413926158f85dcaaebc114dccb9df6d4827e90
klarissee <92704597+klarissee@users.noreply.github.com>
2021-10-27 14:25:14 +0800

    addFunction2.c
    add function working

---
+++ addFunction2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void addItem();
+
+int main()
+{
+	addItem();
+}
+void addItem() //still haven't edited/done mine -Da
+{
+	FILE* fp = fopen("inventorysample.csv", "a+");
+	
+    int ch;
+    unsigned int id;
+    unsigned char quantity;
+    float price;
+    char itemdesc[80], expirydate[80], temp;
+    
+    /* Flushes input buffer from the newline from scanf() */
+ ////   while ( (ch = getchar()) != '\n' && ch != EOF) ;
+    
+    printf("Enter Product ID Number: ");
+    scanf("%d", &id);
+    printf("Enter Product Name: ");
+    scanf("%c", &temp); //temp statement to clear buffer
+    scanf("%[^\n]%*c", itemdesc);
+    printf("Enter Quantity: ");
+    scanf("%d", &quantity);
+    printf("Enter Expiration Date: ");
+    scanf("%c", &temp); //temp statement to clear buffer
+    scanf("%[^\n]%*c", expirydate);
+    printf("Enter Product Price: ");
+    scanf("%f", &price);
+
+  
+    fprintf(fp, "%d,%s,%d,%s,%f\n", id, itemdesc, quantity, expirydate, price);
+ 
+ 
+    /* Flushes input buffer from the newline from scanf() */
+  //  while ( (ch = getchar()) != '\n' && ch != EOF) ;
+ 
+  //  printf("\n\nPress ENTER to continue.\n");
+  //  while ( (ch = getchar()) != '\n' && ch != EOF)
+ //       ;
+    printf("\nItem recorded!");
+ 
+ 	fclose(fp);
+    return 0;
+} 
