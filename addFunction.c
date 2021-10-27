07ccdb963cfbe3f6b05f662bc771d2bad9405284
DaRealDaryl <92732761+DaRealDaryl@users.noreply.github.com>
2021-10-27 14:00:48 +0800

    Update addFunction.c
    Opted to not use unsigned variables to remove bug
    
    -permitted

--- addFunction.c
+++ addFunction.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 void addItem(void) //still haven't edited/done mine -Da
 {
-    int ch;
-    unsigned int id;
-    unsigned char quantity;
+    //int ch;
+    int id, quantity, ch, invcount;
+    //unsigned char quantity, ch, invcount;
     float price;
     char itemdesc[80], expirydate[80], temp;
+    
+    system("cls");
     
     /* Flushes input buffer from the newline from scanf() */
     while ( (ch = getchar()) != '\n' && ch != EOF) ;
     
+    invcount = 0; 
     printf("Enter Product ID Number: ");
-    scanf("%d", &id);
+    scanf("%d", &id); if(id < 0 || id > 99999) { invcount = 1; goto invalid;}
     printf("Enter Product Name: ");
     scanf("%c", &temp); //temp statement to clear buffer
     scanf("%[^\n]%*c", itemdesc);
     printf("Enter Quantity: ");
-    scanf("%d", &quantity);
+    scanf("%d", &quantity);  if(quantity < 1) { invcount = 1; goto invalid;}
     printf("Enter Expiration Date: ");
     scanf("%c", &temp); //temp statement to clear buffer
     scanf("%[^\n]%*c", expirydate);
@@ -26,14 +31,32 @@ void addItem(void) //still haven't edited/done mine -Da
     scanf("%f", &price);
     
     printf("\n\n%d %s %d %s %f", id, itemdesc, quantity, expirydate, price);
- 
- 
+ 	
+ 	//printf("\n\n%u", id);	//tester line
+	invalid:
+		if(invcount == 1) { printf("\nInvalid Input!"); }
+	
     /* Flushes input buffer from the newline from scanf() */
     while ( (ch = getchar()) != '\n' && ch != EOF) ;
  
     printf("\n\nPress ENTER to continue.\n");
     while ( (ch = getchar()) != '\n' && ch != EOF)
         ;
- 
+        
     return;
 } 
+
+/*this is a recycle bin
+
+
+    //id = enterID();
+
+int enterID()
+{
+	unsigned int loID;
+	scanf("%u", &loID);
+	printf("%u", loID);
+	return loID;
+}
+
+*/
