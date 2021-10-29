299c80c3f51545ae0880e8a56a7e8818d7239201
DaRealDaryl <92732761+DaRealDaryl@users.noreply.github.com>
2021-10-29 12:18:12 +0800

    Update addFunction.c
    added zero append on ID to make sure inserted ID is 5 digits, added anti ID duplicate on entries

--- addFunction.c
+++ addFunction.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 void addItem(void)
 {
 	FILE* fp = fopen("inventorysample.csv", "a+");
 	
-    int id, quantity, ch, invcount;
+    int id, quantity, ch, invcount, idlength, i;
     float price;
-    char itemdesc[40], expirydate[11], temp;
+    char itemdesc[40], expirydate[11], temp, stringID[5], readcsv[80], idfromcsv[5];
     
     system("cls");
     
@@ -21,15 +19,71 @@ void addItem(void)
     printf("ADD INVENTORY ITEM\n\nEnter New Product:\n");
     
 	printf("Enter Product ID Number: ");
-	if(scanf("%d%c", &id, &temp) != 2 || temp != '\n')
+	if(scanf("%d%c", &id, &temp) != 2 || temp != '\n')	//checks if Product ID is an int
     	{ invcount = 1; goto invalid;}
 	else
 	{
-		if(id < 0 || id > 99999) { invcount = 1; goto invalid;}
+		if(id < 0 || id > 99999) { invcount = 1; goto invalid; } //checks if Product ID is not less than 0 or greater than 99999
+		else
+		{
+			sprintf(stringID, "%d", id); //converts int id into string and store to stringID
+			//fflush(stdin); // Reset the input buffer
+			//printf("\n%s", stringID); //tester line
+			idlength = strlen(stringID); //gets length of stringID
+			//printf("\n%d", idlength);	//tester line
+			//if(idlength == 4) // //checks if the ID is not a 5 digit number
+			//else { invcount = 1; goto invalid; }
+			
+			switch(idlength)
+			{
+				case 5:
+					break;
+				case 4:
+					for(i = 4; i >= 1; i--)
+						stringID[i] = stringID[i-1];
+					
+					stringID[0] = '0';
+					idlength = strlen(stringID); //gets length of stringID	//tester line
+					break;
+				case 3:
+					for(i = 4; i >= 2; i--)
+						stringID[i] = stringID[i-2];
+					
+					stringID[0] = '0'; stringID[1] = '0'; 
+					idlength = strlen(stringID); //gets length of stringID	//tester line
+					break;
+				case 2:
+					for(i = 4; i >= 3; i--)
+						stringID[i] = stringID[i-3];
+					
+					stringID[0] = '0'; stringID[1] = '0'; stringID[2] = '0'; 
+					idlength = strlen(stringID); //gets length of stringID	//tester line
+					break;
+				case 1:
+					stringID[4] = stringID[0];
+					stringID[0] = '0'; stringID[1] = '0'; stringID[2] = '0'; stringID[3] = '0'; 
+					idlength = strlen(stringID); //gets length of stringID	//tester line
+					break;
+				default: { printf("\nUnexpected Error"); invcount = 1; goto invalid; }
+			}
+			//printf("\n%s", stringID); //tester line
+			//printf("\n%d\n", idlength);	//tester line
+			
+			while(!feof(fp))
+			{
+				fgets(readcsv, 80, fp); // read each line in the inventory
+				//printf("\nThis is read csv: %s", readcsv); //tester line
+				strncpy(idfromcsv, readcsv+1,5); // Copy the 5 digit Product ID of the current line
+				//printf("\nThis is idfromcsv %s", idfromcsv); //tester line
+				if(strcmp(idfromcsv, stringID) == 0) { invcount = 2; goto duplicate; } //compare id from a line in csv to id inputted
+				//printf("%d", strcmp(idfromcsv, stringID)); //tester line
+			}
+		}
+		
 	}
     
 	printf("Enter Product Name: ");
-    scanf("%[^\n]%*c", itemdesc);
+    scanf("%[^\n]%*c", itemdesc);	//scanf for string with spaces
    
     printf("Enter Quantity: ");
     if(scanf("%d%c", &quantity, &temp) != 2 || temp != '\n')
@@ -52,13 +106,16 @@ void addItem(void)
     
     //printf("\n\n%d, %s, %d, %s, %f", id, itemdesc, quantity, expirydate, price);  //redisplays inputs, used for testing
  	
- 	fprintf(fp, "\"%d\",\"%s\",\"%d\",\"%s\",\"%f\"\n", id, itemdesc, quantity, expirydate, price);
+ 	fprintf(fp, "\"%s\",\"%s\",\"%d\",\"%s\",\"%f\"\n", stringID, itemdesc, quantity, expirydate, price);
  	printf("\nItem Recorded!");
  
  	fclose(fp);
 	invalid:
 		if(invcount == 1) { printf("\nInvalid Input!"); }
-	
+		
+	duplicate:
+		if(invcount == 2) { printf("\nID Already Exists!"); }
+		
     /* Flushes input buffer from the newline from scanf() */
     while ( (ch = getchar()) != '\n' && ch != EOF) ;
  
